e7e02dabcf94757f2359f6e14948e950c3d19dfe
Filip Halaxa <filip.halaxa@elnino.cz>
2014-03-12 09:17:51 +0100

    Dev dependency on elnino/mock

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
+if ( ! is_readable(__DIR__ . '/../vendor/autoload.php')) {
+    die (sprintf(
+        'Please install dependencies for this package first by running `composer install` in %s.',
+        dirname(__DIR__)
+    ));
+}
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader->add('ElninoTest', __DIR__ . '/functional');
 $loader->add('ElninoTest', __DIR__ . '/unit');
-
