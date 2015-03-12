c412a20dcf2bd533ae0416facad1dc8e99a3b4a4
Petr Král <kral.petr.88@gmail.com>
2015-03-12 07:50:22 +0000

    Count spec returns single scalar result

--- src/Elnino/DomainQuery/Spec/Count.php
+++ src/Elnino/DomainQuery/Spec/Count.php
@@ -2,9 +2,11 @@
 
 namespace Elnino\DomainQuery\Spec;
 
+use Doctrine\ORM\Query;
 use Doctrine\ORM\Query\Expr\From;
 use Doctrine\ORM\QueryBuilder;
 use Elnino\DomainQuery\QueryBuilderModifierInterface;
+use Elnino\DomainQuery\QueryModifierInterface;
 
 /**
  * Selects count of entities
@@ -13,7 +15,7 @@ use Elnino\DomainQuery\QueryBuilderModifierInterface;
  * @since 2015-01-09
  * @package Elnino\DomainQuery\Spec
  */
-class Count implements QueryBuilderModifierInterface
+class Count implements QueryBuilderModifierInterface, QueryModifierInterface
 {
     /**
      * Adds to QueryBuilder custom parameters, joins etc.
@@ -25,9 +27,18 @@ class Count implements QueryBuilderModifierInterface
     {
         /** @var From $from */
         $from = $qb->getDQLPart("from")[0];
-
         $alias = $from->getAlias();
-
         $qb->select("count({$alias})");
+    }
+
+    /**
+     * Modifies parameters of a query object.
+     *
+     * @param Query $query
+     * @return void
+     */
+    public function modifyQuery(Query $query)
+    {
+        $query->setHydrationMode(Query::HYDRATE_SINGLE_SCALAR);
     }
 }
